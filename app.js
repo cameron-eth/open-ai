04c126a4db800bb0d147b84557351c93c439aa0e
Cameron <110560122+cameron-eth@users.noreply.github.com>
2023-07-03 08:50:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_KEY = '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'
+const API_KEY = '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'
 const submitButton = document.querySelector('#submit')
 const outPutElement = document.querySelector('#output')
 const inputElement = document.querySelector('input')
